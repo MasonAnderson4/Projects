60239d41efc2323bca5f5956498384b09c300b23
MasonAnderson4 <masonator0465@gmail.com>
2024-12-28 22:06:29 -0700

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    tv: `In my freetime I like to watch YouTube, anime, TV shows, and movies.
+    Some of my favorites are Interstellar, Vinland Saga, Naruto, and How I Met 
+    Your Mother.`,
+    fridge: `I love food and trying new things. I like to create many different 
+    protein concoctions to enjoy after working out. I am particularly fond of 
+    my Ninja Creami protein ice cream and I have some almost every day.`,
+    csdegree: `This is my soon to be CS degree. I am expecting to graduate from 
+    Creighton University in May of 2026 with a B.S. in Computer Science and minors
+    in Data Sciene and Economics.`,
+    resume: `This is my resume. Feel free to check it out 
+    <a href="https://github.com/MasonAnderson4/Resume/blob/main/MA%20Resume.pdf" target="_blank">here.</a> You can contact
+    me by email: masonator0465@gmail.com or by phone number: 720-990-8275.`,
+    pc: `This is my PC. I built it myself and enjoy using it for playing games, 
+    watching videos, doing school, and coding projects like this one. 
+    <a href="https://github.com/MasonAnderson4/Projects/tree/master/weather-app" target="_blank">Here</a> you 
+    can find a repo of a recent weather app project I made using JavaScript 
+    and React.`,
+    links: `Here are some links to check out more about me and some of the work
+    I have done. 
+    <a href="https://www.linkedin.com/in/mason-anderson-a7104226a/" target="_blank">LinkedIn</a> 
+    <a href="https://github.com/MasonAnderson4" target="_blank">GitHub</a>`,
+    project: `This project was made with vite and the kaboom.js library
+    using JavaScript. The text is rendered in HTML/CSS.`
+
+};
