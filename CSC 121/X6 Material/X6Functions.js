6f4e27458e37581ec3e29d2b7a3929050a463315
Mason Anderson <149114025+MasonAnderson4@users.noreply.github.com>
2025-02-11 16:45:01 -0600

    Add files via upload

---
+++ CSC 121/X6 Material/X6Functions.js
@@ -0,0 +1,19 @@
+		function the_sum_function(x,y){
+			z = x + y;
+			return z;
+		}
+
+			function the_diff_function(x,y){
+			z = x - y;
+			return z;
+		}
+
+			function the_multi_function(x,y){
+			z = x * y;
+			return z;
+		}
+
+		function the_div_function(x,y){
+			z = x / y;
+			return z;
+		}
